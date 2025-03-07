1d11411115ca789d6f723be490b97051f75bbf44
Emil Öhrström <Emil.ohrstrom@hotmail.com>
2025-03-07 20:51:53 +0100

    Added first basic models

---
+++ ResuMate/ResuMate/Models/EducationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ResuMate.Models
+{
+    public class EducationDto
+    {
+        public string School { get; set; }
+        public string Degree { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+    }
+}
