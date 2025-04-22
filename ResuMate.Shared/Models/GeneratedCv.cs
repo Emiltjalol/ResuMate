ff147b209da98b9fa9d0867ef8ba0e34950a2039
Emil Öhrström <Emil.ohrstrom@hotmail.com>
2025-04-22 13:31:34 +0200

    Lagt till genererat cv table i databas

---
+++ ResuMate.Shared/Models/GeneratedCv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ResuMate.Shared.Models
+{
+    public class GeneratedCv
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        public string UserId { get; set; } = string.Empty;
+        
+    }
+}
--- ResuMate/Data/ApplicationDbContext.cs
+++ ResuMate/Data/ApplicationDbContext.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ResuMate.Shared.Models;
 
 namespace ResuMate.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<GeneratedCv> GeneratedCvs { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        
+        builder.Entity<GeneratedCv>()
+            .HasKey(c => c.Id);
+
+        builder.Entity<GeneratedCv>()
+            .Property(c => c.UserId)
+            .IsRequired();
+    }
 }
