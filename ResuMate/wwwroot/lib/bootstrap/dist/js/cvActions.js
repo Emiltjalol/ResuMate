4ac8d112a7f44403f149ae3bf25555d7d2841bcf
Emil Öhrström <Emil.ohrstrom@hotmail.com>
2025-05-23 15:27:37 +0200

    Gjort om i sidan som visar upp sparade cvn, gjort om funktionen och stylingen.

---
+++ ResuMate/wwwroot/lib/bootstrap/dist/js/cvActions.js
@@ -0,0 +1,20 @@
+﻿window.viewPdf = (base64, title) => {
+    const newWindow = window.open();
+    if (newWindow) {
+        newWindow.document.write(`
+            <iframe src="data:application/pdf;base64,${base64}"
+                    style="width: 100%; height: 100%; border: none;"
+                    title="${title}">
+            </iframe>
+        `);
+    }
+};
+
+window.downloadPdf = (base64, fileName) => {
+    const link = document.createElement('a');
+    link.href = 'data:application/pdf;base64,' + base64;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
